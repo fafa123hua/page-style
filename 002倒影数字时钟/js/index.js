61291fe01ef217532d2b3b08f27ba07bca1f632c
fafa123hua <65300014+fafa123hua@users.noreply.github.com>
2021-10-21 00:56:31 +0800

    fix: 完善小bug

--- 002倒影数字时钟/js/index.js
+++ 002倒影数字时钟/js/index.js
@@ -1,13 +1,13 @@
+var hours = document.getElementsByClassName('a')
+var minutes = document.getElementsByClassName('b')
+var seconds = document.getElementsByClassName('c')
 function clock() {
-  var hours = document.getElementsByClassName('hour')
-  var minutes = document.getElementsByClassName('minutes')
-  var seconds = document.getElementsByClassName('seconds')
   var h = new Date().getHours()
   var m = new Date().getMinutes()
   var s = new Date().getSeconds()
-  hours.innerHTML = h
-  minutes.innerHTML = m
-  seconds.innerHTML = s
+  hours[0].innerHTML = h < 10 ? '0' + h : h
+  minutes[0].innerHTML = m < 10 ? '0' + m : m
+  seconds[0].innerHTML = s < 10 ? '0' + s : s
   console.log(hours, minutes, seconds)
 }
 setInterval(clock, 1000)
