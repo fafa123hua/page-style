6b11fcee88afa34056d86f13fb57935ad79969ab
fafa123hua <65300014+fafa123hua@users.noreply.github.com>
2021-11-28 00:36:07 +0800

    fix: 移动和点击冲突

--- 043模态框/js/index.js
+++ 043模态框/js/index.js
@@ -1,30 +1,28 @@
 const toggle = document.getElementsByClassName('toggle')
 var timeOutId = null
-let bool = 1
-boxMove(toggle[0])
-function boxMove(node) {
-  node.onmousedown = function (ev) {
+var bool = 1
+toggle[0].onmousedown = function (ev) {
+  e = ev || window.event
+  let x = e.pageX - this.offsetLeft
+  let y = e.pageY - this.offsetTop
+  bool = e.pageX
+  var move = (document.onmousemove = (ev) => {
     e = ev || window.event
-    let x = e.pageX - this.offsetLeft
-    let y = e.pageY - this.offsetTop
-    var move = (document.onmousemove = (ev) => {
-      e = ev || window.event
-      this.style.left = e.pageX - x + 'px'
-      this.style.top = e.pageY - y + 'px'
+    this.style.left = e.pageX - x + 'px'
+    this.style.top = e.pageY - y + 'px'
+    if (bool - e.pageX != 0) {
+      bool = 0
+    }
+    document.onmousemove = null
+    timeOutId = setTimeout(function () {
+      document.onmousemove = move
+    }, 90)
+    document.onmouseup = () => {
+      clearTimeout(timeOutId)
       document.onmousemove = null
-      timeOutId = setTimeout(function () {
-        document.onmousemove = move
-      }, 90)
-      document.onmouseup = () => {
-        clearTimeout(timeOutId)
-        document.onmousemove = null
-      }
-    })
-    if (bool) {
-      toggle[0].onclick = function () {
+      if (bool) {
         toggle[0].classList.toggle('active')
       }
     }
-    bool = 1
-  }
+  })
 }
